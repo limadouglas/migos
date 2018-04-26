5814cdc983fc70aadf7243f8456577d9f487f85c
Monique Altero <monique.elen@boavistaservicos.com.br>
2018-04-26 14:57:46 -0300

    Segunda versão do aplicativo

---
+++ js/routes.js
@@ -0,0 +1,51 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('configuraEs', {
+    url: '/config',
+    templateUrl: 'templates/configuraEs.html',
+    controller: 'configuraEsCtrl'
+  })
+
+  .state('chat', {
+    url: '/chat',
+    templateUrl: 'templates/chat.html',
+    controller: 'chatCtrl'
+  })
+
+  .state('vingadores', {
+    url: '/infoevento',
+    templateUrl: 'templates/vingadores.html',
+    controller: 'vingadoresCtrl'
+  })
+
+  .state('feedDeEventos', {
+    url: '/feedeventos',
+    templateUrl: 'templates/feedDeEventos.html',
+    controller: 'feedDeEventosCtrl'
+  })
+
+  .state('meusEventos', {
+    url: '/meuseventos',
+    templateUrl: 'templates/meusEventos.html',
+    controller: 'meusEventosCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+
+});
